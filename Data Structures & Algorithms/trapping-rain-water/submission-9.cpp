9225b9843f9188f6fe25cc3015f46c95880f36f1
Minh Duc Nguyen <minhduc3051999@gmail.com>
2026-05-26 16:20:09 +0700

    Add: trapping-rain-water - submission-9

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-9.cpp
@@ -0,0 +1,50 @@
+class Solution {
+   public:
+    int trap(vector<int>& height) {
+        int left = 0;
+        int right = left + 1;
+        int results = 0;
+        int temp = 0;
+        bool is_reverse = false;
+        int reverse_target = 0;
+
+        while (right < height.size()) {
+            if (height[left] > height[right]) {
+                temp += height[left] - height[right];
+                right++;
+
+                if (right == height.size() && height[left] > height[right - 1]) {
+                    is_reverse = true;
+                    reverse_target = left;
+                }
+            } else if (height[left] <= height[right]) {
+                results += temp;
+                temp = 0;
+                left = right;
+                right = right + 1;
+            }
+        }
+
+        if (is_reverse) {
+            left = height.size() - 1;
+            right = left - 1;
+            temp = 0;
+
+            cout << reverse_target << endl;
+            while (right >= reverse_target) {
+                if (height[left] > height[right]) {
+                    temp += height[left] - height[right];
+                    right--;
+                } else if (height[left] <= height[right]) {
+                    cout << temp << endl;
+                    results += temp;
+                    temp = 0;
+                    left = right;
+                    right = right - 1;
+                }
+            }
+        }
+
+        return results;
+    }
+};
