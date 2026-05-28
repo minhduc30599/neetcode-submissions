b27d3c36f783e9cbd49ad565a57cddc74cf06ad2
Minh Duc Nguyen <minhduc3051999@gmail.com>
2026-05-28 17:00:14 +0700

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.cpp
@@ -0,0 +1,24 @@
+class Solution {
+   public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        int n = position.size();
+        stack<double> st;
+        vector<pair<int, int>> vec(n);
+
+        for (int i = 0; i < n; i++) {
+            vec[i] = { position[i], speed[i] };
+        }
+
+        sort(vec.rbegin(), vec.rend());
+
+        for (int i = 0; i < n; i++) {
+            double time = (double)(target - vec[i].first) / vec[i].second;
+
+            if (st.empty() || !st.empty() && st.top() < time) {
+                st.push(time);
+            }
+        }
+
+        return st.size();
+    }
+};
